01ac2ad6f60a3c175de9cd83f7c90b20d20eaecd
AmmAr MouRad <amrmrd111spcl@hotmail.com>
2018-07-03 22:08:40 +0300

    v0.0.7

--- object.ts
+++ object.ts
@@ -460,21 +460,21 @@ export function getArrayChanges(arr1: any[], arr2: any[], key: string): { count:
   let arrRef1 = arr1.length > arr2.length ? arr1 : arr2;
   let arrRef2 = arr1.length > arr2.length ? arr2 : arr1;
   let swipe = arrRef1 === arr1 ? false : true;
-  let results: any = [];
+  let results: any = {};
 
   for (let i = 0; i < arrRef1.length; i++) {
     let compareElm = arrRef2.find(elm => elm[key] === arrRef1[i][key]) || null;
     let compareResults = getChanges(swipe ? compareElm : arrRef1[i], swipe ? arrRef1[i] : compareElm);
 
     if (compareResults.count > 0)
-      results.push({ _id: arrRef1[i][key], ...compareResults });
+      results[arrRef1[i][key]] = compareResults;
   }
 
   for (let i = 0; i < arrRef2.length; i++) {
     let compareElm = arrRef1.find(elm => elm[key] === arrRef2[i][key]) || null;
 
     if (!compareElm) {
-      results.push({ _id: arrRef2[i][key], count: 1, changes: [swipe ? arrRef2[i] : null, swipe ? null : arrRef2[i]] });
+      results[arrRef1[i][key]] = { count: 1, changes: [swipe ? arrRef2[i] : null, swipe ? null : arrRef2[i]] };
     }
   }
 
