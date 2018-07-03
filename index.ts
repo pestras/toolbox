15549af55182923a7ddb69d1c2f8d03d44a2725b
AmmAr MouRad <amrmrd111spcl@hotmail.com>
2018-07-03 12:41:59 +0300

    v0.0.5

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import * as stringUtil from './string';
 import * as arrayUtil from './array';
 import * as objectUtil from './object';
+import * as crypt from './crypt';
 import { Unique } from './unique';
 
 export default {
   ...stringUtil,
   ...arrayUtil,
   ...objectUtil,
+  ...crypt,
   Unique
 }
