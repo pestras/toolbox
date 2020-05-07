dbb912df8a983a1806ee594bd365ef04cee64201
pestras <admin@pestras.com>
2020-05-07 14:20:30 +0300

    init

---
+++ url/index.ts
@@ -0,0 +1,150 @@
+import { URL as RootURL } from 'url';
+import { parse } from '../object/parse';
+import { stringify } from '../string/stringify';
+import { strSplice } from '../string/str-splice';
+import { PathPattern } from './path-pattern';
+
+function cleanParam(param: string): string {
+  param = param.slice(1);
+  if (param.charAt(param.length - 1) === '?')
+    param = param.slice(0, param.length - 1);
+  return param;
+}
+
+export class URL extends RootURL {
+  queryObject: any;
+  private _pathPattern: PathPattern;
+
+  constructor(href: string, base?: string | RootURL) {
+    super(href, base);
+
+    this.queryObject = this.search ? URL.QueryToObject(this.search.slice(1)) : {};
+  }
+
+  static QueryToObject(query: string): any {
+    query = decodeURI(query);
+
+    let result: any = {};
+    let parts: any[] = query.split('&');
+
+    for (let i = 0; i < parts.length; i++) {
+      let part: any = parts[i];
+      let pair: any[] = part.split('=');
+
+      result[pair[0]] = pair[1] ? parse(pair[1]) : null;
+    }
+
+    return result;
+  }
+
+  static ObjectToQuery(obj: { [key: string]: any }, encode?: boolean): string {
+    let result = "";
+  
+    if (!obj || !Object.keys(obj).length)
+      return result;
+  
+    for (let prop in obj)
+      result += '&' + prop + '=' + stringify(obj[prop]);
+  
+    return encode ? encodeURI(result.slice(1)) : result.slice(1);
+  }
+
+  static Clean(path: string) {
+    if (!path)
+
+      return "";
+
+    path = path.trim()
+      .replace(/^\/|\/$/g, "")
+      .replace(/:\/\//, '%')
+      .replace(/\/{2,}/g, "/")
+      .replace(/(\w+)\/\.+/g, "$1")
+      .replace('%', '://');
+
+    return path;
+  }
+
+  static From(origin: string, pathname?: { pattern: string, params: { [key: string]: string } } | string, searchObj?: any, hash?: string): URL {
+    let url = new URL(origin);
+    if (pathname) {
+      if (typeof pathname === 'string')
+        url.pathname = pathname;
+      else {
+        let pathPattern = new PathPattern(pathname.pattern);
+        let path = pathPattern.pathFromParams(pathname.params);
+
+        if (pathPattern.match(path)) {
+          url.pathname = path;
+          url._pathPattern = pathPattern;
+        }
+      }
+    }
+
+    if (searchObj) {
+      url.search = URL.ObjectToQuery(searchObj);
+    }
+
+    if (hash)
+      url.hash = hash;
+
+    return url;
+  }
+
+  set pathname(value: string) {
+    value = PathPattern.Clean(value);
+
+    if (this._pathPattern) {
+      if (this._pathPattern.match(value))
+        super.pathname = '/' + value;
+    } else {
+      super.pathname = '/' + value;
+    }
+  }
+
+  get pathname(): string {
+    return super.pathname;
+  }
+
+  get pathPattern(): string {
+    return this._pathPattern ? this._pathPattern.pattern : null;
+  }
+
+  set pathPattern(value: string) {
+    let pp = new PathPattern(value);
+    if (!pp.match(super.pathname))
+      throw 'invalid existing pathname';
+
+    this._pathPattern = pp;
+  }
+
+  get params(): { [key: string]: string } {
+    return this._pathPattern ? this._pathPattern.params : {};
+  }
+
+  set params(value: { [key: string]: string }) {
+    if (this._pathPattern) {
+      let params = this.params;
+      Object.assign(params, value);
+      super.pathname = this._pathPattern.pathFromParams(value);
+    }
+  }
+
+  setPathParam(key: string, value: string) {
+    if (this._pathPattern) {
+      let params = this.params;
+      params[key] = value;
+      this.params = params;
+    }
+  }
+
+  isSecure(): boolean {
+    return this.protocol.charAt(this.protocol.length - 2) === 's';
+  }
+
+  secure(enable: boolean = true) {
+    if (enable && !this.isSecure())
+      this.protocol = strSplice(this.protocol, this.protocol.length - 1, 0, 's');
+    else if (!enable && this.isSecure())
+      this.protocol = strSplice(this.protocol, this.protocol.length - 2, 1);
+  }
+}
---
+++ url/path-pattern.ts
@@ -0,0 +1,146 @@
+export interface PathPart {
+  text: string;
+  regex?: RegExp;
+  isParam?: boolean;
+  isRest?: boolean;
+  isOptional?: boolean;
+}
+
+export class PathPattern {
+  private _parts: PathPart[];
+  private _pattern: string;
+  private _hasRest = false;
+  private _hasOptionalRest = false;
+  private _optionalsCount = 0;
+  private _values: { [key: string]: string } = {};
+
+  constructor(pattern: string) {
+    this._pattern = PathPattern.Clean(pattern);
+    this.init();
+  }
+
+  static Clean(path: string) {
+    if (!path)
+      return "";
+
+    path = path.trim()
+      .replace(/^\//g, "")
+      .replace(/\/$/g, "")
+      .replace(/\/{2,}/g, "/")
+      .replace(/(\w+)\/\.+/g, "$1");
+
+    return path;
+  }
+
+  private init() {
+    this._parts = [];
+    this._hasRest = false;
+    this._hasOptionalRest = false;
+    this._optionalsCount = 0;
+    this._values = {};
+    let parts = this._pattern.split('/');
+
+    for (let part of parts) {  
+      let pathPart = <PathPart>{ text: part };
+
+      if (part.charAt(0) === '*') {
+        if (this._optionalsCount > 0) throw 'invalidPathPattern';
+        pathPart.isParam = true;
+        pathPart.isRest = true;
+        this._parts.push(pathPart);
+        this._hasRest = true;
+        this._hasOptionalRest = pathPart.isOptional = part.charAt(1) === '?';
+        continue;
+      }
+
+      if (this._hasRest) throw 'invalidPathPattern';
+
+      if (part.charAt(0) !== '{') {
+        if (this._optionalsCount > 0) throw 'invalidPathPattern';
+        this._parts.push(pathPart);
+        continue;
+      };
+      
+      pathPart.isParam = true;
+      pathPart.isOptional = part.charAt(part.length - 1) === '?';
+      pathPart.isOptional && this._optionalsCount++;
+
+      let block = part.slice(1, pathPart.isOptional ? -2 : -1).split(':');
+      pathPart.text = block[0]
+      if (block.length > 1) pathPart.regex = new RegExp(block[1], block[2]);
+      this._parts.push(pathPart);
+    }
+  }
+
+  get params(): { [key: string]: string } { return Object.assign({}, this._values); }
+  get pattern(): string { return this._pattern; }
+
+  set pattern(value: string) {
+    this._pattern = PathPattern.Clean(value);
+    this.init();
+  }
+
+  match(path: string) {
+    path = PathPattern.Clean(path);
+    let pathParts = path.split('/');
+
+    if (this._hasRest) {
+      if (this._hasOptionalRest) {
+        if (pathParts.length < this._parts.length - 1) return false;
+      } else if (pathParts.length < this._parts.length) return false;
+
+    } else if (this._optionalsCount > 0) {
+      if (this._parts.length < pathParts.length) return false;
+      else if (this._parts.length - this._optionalsCount > pathParts.length) return false;
+
+    } else if (pathParts.length !== this._parts.length) return false;
+
+    for (let i = 0; i < this._parts.length; i++) {
+      let part = this._parts[i];
+
+      if (!part.isParam) {
+        if (part.text !== pathParts[i]) return false;
+
+      } else {
+        if (part.isRest) {
+          this._values[part.text] = pathParts.slice(i).join('/') || null;
+
+        } else {
+          let value = pathParts[i] || null;
+          if (value && part.regex)
+            if (!part.regex.test(value)) return false;
+          this._values[part.text] = value;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  pathFromParams(params: { [key: string]: string }): string {
+    let path = "/";
+
+    for (let part of this._parts) {
+      if (!part.isParam) path += part.text + '/';
+      else {
+        if (part.isRest) {
+          let value = params[part.text];
+          if (!value && !part.isOptional) throw 'rest is required';
+          path += params[part.text] || '';
+          
+        } else {
+          let value = params[part.text];
+
+          if (!value && !part.isOptional) throw `${part.text} param is required`;
+
+          if (value && part.regex)
+            if (!part.regex.test(value)) throw `invalid param value ${value}`;
+          
+            path += value + '/' || '';
+        }
+      }
+    }
+
+    return PathPattern.Clean(path);
+  }
+}
