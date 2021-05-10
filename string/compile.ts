e0f823faebbdbd07cd4e6c8cd7397e7c4dd741d4
pestras <admin@pestras.com>
2021-05-10 23:59:35 +0300

    update string compile with multiple sources

--- string/compile.ts
+++ string/compile.ts
@@ -1,21 +1,29 @@
-export function compile(template: string, data: { [key: string]: any }, skip = false): string {
+export function compile(template: string, ...data: { [key: string]: any }[]): string {
   let reg = /\{\{\s*([\w\.]+)\s*\}\}/g;
+  let skip = data.length > 1;
 
-  return template.replace(reg, (match: string, $1: string): string => {
-    let parts = $1.split("."), temp: any;
-    match = match;
+  for (let i = 0; i < data.length; i++) {
+    let source = data[i];
+    skip = i < data.length - 1;
 
-    if (parts.length == 1) {
-      let value = data[parts[0]];
-      return value === undefined ? (skip ? `{{${$1}}}` : "") : value;
-    }
+    template = template.replace(reg, (match: string, $1: string): string => {
+      let parts = $1.split("."), temp: any;
+      match = match;
+  
+      if (parts.length == 1) {
+        let value = source[parts[0]];
+        return value === undefined ? (skip ? `{{${$1}}}` : "") : value;
+      }
+  
+      temp = source[parts[0]];
+  
+      for (let i = 1; i < parts.length; i++) {
+        temp = temp[parts[i]];
+      }
+  
+      return temp === undefined ? (skip ? `{{${$1}}}` : "") : temp;
+    });
+  }
 
-    temp = data[parts[0]];
-
-    for (let i = 1; i < parts.length; i++) {
-      temp = temp[parts[i]];
-    }
-
-    return temp === undefined ? (skip ? `{{${$1}}}` : "") : temp;
-  });
+  return template;
 }
